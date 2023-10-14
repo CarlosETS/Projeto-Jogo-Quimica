a8b4857294fa900e149c8ad3b7d796a7c3d80064
CarlosETS <mrduingles@gmail.com>
2023-10-14 19:53:27 -0300

    Ajustes

--- Front/src/routes.jsx
+++ Front/src/routes.jsx
@@ -5,7 +5,7 @@ import { CircularProgress } from '@mui/material';
 import Home from './pages/home.jsx'
 import ChemistryGame from './pages/chemistrygame.jsx'
 import Ranking from './pages/ranking.jsx';
-import AddQuestions from './pages/addquetion.jsx';
+import AddQuestions from './pages/addquestion.jsx';
 import SaltFormation from './pages/saltformation.jsx';
 
 // const Componente = lazy(() => import('./pages/Auth/Login'))
