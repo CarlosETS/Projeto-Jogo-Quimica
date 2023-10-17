c2697071b2cfe3ade38184f032f6715b9a7a2fd2
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-10-16 22:10:31 -0300

    refactor: mudanças nas perguntas

--- Front/src/components/Option.jsx
+++ Front/src/components/Option.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { QuizContext } from "../context/quiz.jsx";
 
@@ -6,12 +6,27 @@ import "../assets/option.css";
 
 const Option = ({ option, selectOption, answer, hide }) => {
     const [quizState, dispatch] = useContext(QuizContext);
+    const [clicked, setClicked] = useState(false);
+
+    const isCorrect = quizState.answerSelected && option === answer;
+    const isWrong = quizState.answerSelected && option !== answer;
+
+    const handleOptionClick = () => {
+        if (!clicked) {
+            setClicked(true);
+            if (isWrong) {
+                dispatch({ type: "DISABLE_OPTIONS" }); // Desativa outras opções após clicar em uma incorreta
+            }
+        }
+    }
 
     return (
         <div
-            onClick={() => selectOption()}
+            onClick={() => handleOptionClick()}
             className={`option ${quizState.answerSelected && option === answer ? "correct" : ""}
                 ${quizState.answerSelected && option !== answer ? "wrong" : ""} ${hide ? "hide" : ""}`}>
+            {isCorrect && <span className="checkmark" />}
+            {isWrong && !quizState.answerSelected && <span className="crossmark" />}
             <p>{option}</p>
         </div>
     );
