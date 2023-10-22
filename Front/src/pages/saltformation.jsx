36048b6739afeaf21225c4a4dc7944080a8175a6
Carlos Eduardo <mrduingles@gmail.com>
2023-10-22 12:08:56 -0300

    refactor: Recriação da tela de salt formation

--- Front/src/pages/saltformation.jsx
+++ Front/src/pages/saltformation.jsx
@@ -1,50 +1,59 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import ListItem from '../components/ItemList.jsx';
+import '../assets/saltformation.css';
 
-import Grid from "@mui/material/Grid";
-import Controls from "../components/ControlsForm.jsx";
-import Operator from "../components/Operator.jsx";
-import { DragDropContext } from "react-beautiful-dnd";
+function App() {
+  const [items, setItems] = useState([
+    { id: '1', content: 'NaOH' },
+    { id: '2', content: 'H2O' },
+    { id: '3', content: 'Na' },
+    { id: '4', content: 'OH' },
+    { id: '5', content: '+' },
+    { id: '6', content: '+' },
+    { id: '7', content: '->' },
+  ]);
 
-function SaltFormation() {
-  const [operatorData, setOperatorData] = useState([]);
+  const onDragEnd = (result) => {
+    if (!result.destination) return; // Dropped outside the list
 
-  const onDragEnd = (data) => {
-    const { draggableId, source, destination } = data;
-    if (source && destination) {
-      if (source.droppableId === "controls_droppable") {
-        const newFormControl = {
-          id: `${operatorData.length}`,
-          type: draggableId,
-          config: {}
-        };
-        const newOperatorData = [...operatorData];
-        newOperatorData.splice(destination.index, 0, newFormControl);
-        setOperatorData(newOperatorData);
-      }
-      if (source.droppableId === "operator_droppable") {
-        if (source.index !== destination.index) {
-          const newOperatorData = [...operatorData];
-          const movedFormControl = newOperatorData.splice(source.index, 1)[0];
-          newOperatorData.splice(destination.index, 0, movedFormControl);
-          console.log(newOperatorData);
-          setOperatorData(newOperatorData);
-        }
-      }
-    }
+    const reorderedItems = Array.from(items);
+    const [movedItem] = reorderedItems.splice(result.source.index, 1);
+    reorderedItems.splice(result.destination.index, 0, movedItem);
+
+    setItems(reorderedItems);
+  };
+
+  const checkOrder = () => {
+    const correctOrder = items.map((item, index) => `Item ${index + 1}`);
+    const isCorrect = JSON.stringify(items.map((item) => item.content)) === JSON.stringify(correctOrder);
+    alert(isCorrect ? 'A ordem está correta!' : 'A ordem está incorreta!');
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Grid spacing={2} direction="row" container>
-        <Grid item>
-          <Controls />
-        </Grid>
-        <Grid item>
-          <Operator operatorData={operatorData} />
-        </Grid>
-      </Grid>
-    </DragDropContext>
+    <div className="App">
+      <h1>Arraste e Solte (Drag and Drop) e Verificação de Ordem</h1>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="list" direction="horizontal">
+          {(provided) => (
+            <ul
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="list-container"
+            >
+              {items.map((item, index) => (
+                <ListItem key={item.id} item={item} index={index} />
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+      <button onClick={checkOrder} className="check-order-button">
+        Verificar Ordem
+      </button>
+    </div>
   );
 }
 
-export default SaltFormation;
+export default App;
